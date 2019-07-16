9947e461cf0c11e25b6c11ec5b39aec81cb771d3
xima <eduardobrunaldi@protonmail.com>
2019-07-16 18:43:57 -0300

    fix word

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ int main() {
   int pointX = 0;
   int pointY = 0;
   int width = 500;
-  int heigth = 500;
+  int height = 500;
 
   cv::Mat img;
-  ScreenShot screen(pointX, pointY, width, heigth);
+  ScreenShot screen(pointX, pointY, width, height);
   screen(img);
   cv::imshow("image", img);
   cv::waitKey(0);
